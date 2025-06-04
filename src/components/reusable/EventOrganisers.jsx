fd5673d2dd9c771bb81a4e7905da079a371cd53b
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2025-06-04 10:57:31 +0200

    wip: past events padding

--- src/components/reusable/EventOrganisers.jsx
+++ src/components/reusable/EventOrganisers.jsx
@@ -9,7 +9,7 @@ export default function EventOrganisers() {
 
   return (
     <section className="py-16 px-6 md:px-12">
-      <h2 className="text-2xl md:text-3xl font-bold text-center mb-14 tracking-tight">
+      <h2 className=" pl-22 py-10 text-2xl sm:text-3xl md:text-4xl font-bold text-black">
         Meet The Organisers
       </h2>
 
