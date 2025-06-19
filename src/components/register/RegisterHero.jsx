50e57fd7a7badf88d04799ee821a5264987ce31a
Mosobe Lulu <mosobelulu@gmail.com>
2025-06-19 22:22:49 +0200

    wip: register page- hero img

--- src/components/register/RegisterHero.jsx
+++ src/components/register/RegisterHero.jsx
@@ -2,15 +2,9 @@ import CurrentYear from "../reusable/CurrentYear";
 
 export default function RegisterHero(){
     return (
-    <section className="relative h-screen w-full overflow-hidden">
-      <video
-        className="absolute top-0 left-0 w-full h-full object-cover brightness-35"
-        src="/videos/registerHero.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      ></video>
+    <section className="relative h-screen w-full overflow-hidden"
+     style={{ backgroundImage: "url('/images/values/Image3.png')" }}>
+            <div className="absolute inset-0 z-0 bg-gradient-to-r from-black/70 via-black/60 to-transparent"></div>
 
       <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center text-center px-4">
         <h1 className="text-4xl md:text-5xl font-bold text-white">
