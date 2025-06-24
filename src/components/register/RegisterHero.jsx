dd59b22228f9042e7cd39f10e0582dc38e8bcd3d
Mosobe Lulu <mosobelulu@gmail.com>
2025-06-24 11:07:13 +0200

    wip: translate register hero

--- src/components/register/RegisterHero.jsx
+++ src/components/register/RegisterHero.jsx
@@ -1,6 +1,9 @@
 import CurrentYear from "../reusable/CurrentYear";
+import { useTranslation } from "react-i18next";
+import Register_button from "../reusable/Register_button"
 
 export default function RegisterHero(){
+  const {t} = useTranslation();
     return (
     <section className="relative h-screen w-full overflow-hidden"
      style={{ backgroundImage: "url('/images/values/Image3.png')" }}>
@@ -8,17 +11,14 @@ export default function RegisterHero(){
 
       <div className="absolute inset-0 bg-black/50 flex flex-col justify-center items-center text-center px-4">
         <h1 className="text-4xl md:text-5xl font-bold text-white">
-          Be Part of IndabaX AI <CurrentYear/>
+          {t("registerHeroTitle")}<CurrentYear/>
         </h1>
         <p className="text-lg md:text-xl text-gray-200 mt-4 max-w-2xl">
-          Engage. Learn. Network. Join Africa's leading AI community.
+          {t("registerHeroSubtitle")}
         </p>
-        <a
-          href="#register-form"
-          className="mt-6 bg-white text-black px-6 py-3 rounded-full font-semibold hover:bg-gray-200 transition"
-        >
-          Register Now
-        </a>
+        <div className="mt-10">
+        <Register_button />
+        </div>
       </div>
     </section>
   );
